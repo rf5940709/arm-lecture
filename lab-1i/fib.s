e0cfec6d60b59e17b7a306c90c87f876def4f5d6
rf5940709 <rf5_g3129078@yahoo.com.tw>
2015-10-26 15:34:30 +0800

    Homework 1 non-recursive version Update

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -12,32 +12,25 @@ fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
 	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+ 	
+ 	mov r3, r0			@count 計數迴圈次數
+ 	mov r4, #0
+ 	mov r5,	#1
+ 	
+ 	cmp r0,#1			@if r0>1
+ 	bgt .LOOP			@goto loop
+ 	pop {r3, r4, r5, pc}		@return r0
+ 
+ .LOOP:
+ 	adds r0, r4, r5			@fibonacci f(n-1)+f(n-2)
+ 	mov r4, r5
+ 	mov r5, r0
+ 	subs r3, r3, #1			@count -=1
+ 	cmp r3, #1			@if count>1
+ 	bgt .LOOP	
+ 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
